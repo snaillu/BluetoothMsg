d379dab861b7918702e4e013107d21d60fcfc4ee
snaillu <luqu@live.cn>
2014-10-30 16:16:29 +0800

    Modify

--- BluetoothMsg/src/com/keda/msg/ConsoleUI.java
+++ BluetoothMsg/src/com/keda/msg/ConsoleUI.java
@@ -1,10 +1,30 @@
 package com.keda.msg;
 
-import com.keda.msg.ConnectionProtocol;
+import java.util.Scanner;
 
 public class ConsoleUI implements ConnectionProtocol.EventHandler {
 
 	private ConnectionProtocol protocol;
+	
+	public ConsoleUI(){
+		new Thread(){
+			public void run(){
+				while(true){
+					try{
+						Scanner sc = new Scanner(System.in);
+						String input = sc.next();
+						if(input.equalsIgnoreCase("exit")){
+							break;
+						}else{
+							protocol.broadcastMessage(input);
+						}
+					}catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
+	};
 	
 	public void addMessage(String userName,String msg){
 		System.out.println("msg from user:"+userName +"  msg:"+msg);
